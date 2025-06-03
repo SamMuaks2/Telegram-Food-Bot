5865b5b367ec737d513d74f3ff6f483880604434
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-06-03 15:07:16 +0000

    Call bot properly

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,6 +1,13 @@
 import { Telegraf } from "telegraf";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
 
+bot.catch((err, ctx) => {
+    console.error('Bot error:', err);
+  });
+  
 bot.launch();
