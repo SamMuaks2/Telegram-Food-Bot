9e799d69141ac07bedae4133ee0090c46d8d9cb3
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-06-04 11:00:35 +0000

    Add web link

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,9 +2,13 @@ import { Telegraf } from "telegraf";
 import dotenv from 'dotenv';
 dotenv.config();
 
+const web_link = 'https://telegram-food-bot.vercel.app/';
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
+bot.start((ctx) => ctx.reply('Welcome', {
+  reply_markup: {keyboard: [[{text: "web app", web_app: web_link}]]},
+}));
 
 bot.catch((err, ctx) => {
     console.error('Bot error:', err);
