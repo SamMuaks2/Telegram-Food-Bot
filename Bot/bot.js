df966fcce5139c33523028e1f2f96396458ed68e
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-06-04 11:04:55 +0000

    Add web link

--- Bot/bot.js
+++ Bot/bot.js
@@ -7,7 +7,7 @@ const web_link = 'https://telegram-food-bot.vercel.app/';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome', {
-  reply_markup: {keyboard: [[{text: "web app", web_app: web_link}]]},
+  reply_markup: {keyboard: [[{text: "web app", web_app: {url: web_link}}]]},
 }));
 
 bot.catch((err, ctx) => {
